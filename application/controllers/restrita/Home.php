ff4c0b44f4ae1d44777fc6db1c5668828657dcdb
Tavilo Breno <tavilo.breno10@gmail.com>
2024-08-18 00:39:32 -0300

    Fatiando o template da área administrativa

---
+++ application/controllers/restrita/Home.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') or exit('Ação não permitida');
+
+class Home extends CI_Controller
+{
+	public function index()
+	{
+		$this->load->view('restrita/layout/header');
+		$this->load->view('restrita/home/index');
+		$this->load->view('restrita/layout/footer');
+	}
+
+}
---
+++ application/views/restrita/home/index.php
@@ -0,0 +1,20 @@
+
+	<div class="main-wrapper main-wrapper-1">
+
+		<?php $this->load->view('restrita/layout/navbar'); ?>
+
+		<?php $this->load->view('restrita/layout/sidebar'); ?>
+
+		<!-- Main Content -->
+		<div class="main-content">
+			<section class="section">
+				<div class="section-body">
+
+					<h1>Home da área restrita</h1>
+
+				</div>
+			</section>
+
+			<?php $this->load->view('restrita/layout/setting_sidebar'); ?>
+
+		</div>
